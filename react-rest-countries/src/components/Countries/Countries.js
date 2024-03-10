0bfb63cf6ecfcdb51465db1940696f4fdb5d009e
ShovanTheMan <shovan2192@gmail.com>
2024-03-10 14:51:46 +0600

    key added

--- react-rest-countries/src/components/Countries/Countries.js
+++ react-rest-countries/src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 // rsc
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -13,9 +14,11 @@ const Countries = () => {
     return (
         <div>
            <h2>Hello from Countries: {countries.length}</h2>
+           <div className='countries-container'>
            {
            countries.map(country => <Country
             country={country}
+            key={country.cca3}
             //  name={country.name.common} 
             //  population={country.population} 
             //  area={country.area} 
@@ -23,6 +26,7 @@ const Countries = () => {
              >
              </Country> )
            } 
+           </div>
         </div>
     );
 };
